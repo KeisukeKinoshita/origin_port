2c90fde43850fea6e727f4759de0c4afd3c7e2cb
EC2 Default User <ec2-user@ip-172-31-41-115.ec2.internal>
2018-03-19 11:33:18 +0900

    work edit

--- category-work.php
+++ category-work.php
@@ -3,6 +3,26 @@
 Template Name: work
 */
  get_header(); ?>
+<?php
+function myslider( $postid, $pcsp ) {
+    ?>
+        						<div class="<?php echo $pcsp; ?>">
+                                    <div class="owl-carousel owl-theme">
+                                        <?php  
+                                        $slides= get_post_meta($postid, $pcsp, false);　?>
+                                        <!--もし$slidesにpcという名のimageファイルがあるならwhile処理で吐き出させる-->
+                                        <?php foreach($slides as $slide){
+										echo "<div>";
+										echo wp_get_attachment_image($slide);
+										echo "</div>";
+                                        }?>
+                                    </div>
+                                </div>
+    <?php
+
+}
+
+?>
 <article class="work">
     <div class="container">
         <div class="row">
@@ -15,33 +35,16 @@ Template Name: work
 				<div class="row">
 				<div  class="twelve columns">
     				    <div class="workbox">
-        						<div class="pc">
-                                    <div class="owl-carousel owl-theme">
-                                        <?php  
-                                        $slides = get_post_meta($post->ID,"pc",false);　?>
-                                        <!--もし$slidesにpcという名のimageファイルがあるならwhile処理で吐き出させる-->
-                                        <?php foreach($slides as $slide){
-										echo "<div>";
-										echo wp_get_attachment_image($slide);
-										echo "</div>";
-                                        }?>
-                                    </div>
-                                </div>
-                                <div class="sp">
-                                    <div class="owl-carousel owl-theme">
-                                       <?php  $slide = get_post_meta($post->ID,"sp",false);?>
-                                       <!--もし$slideにspという名のimageファイルがあるならwhile処理で吐き出させる-->
-                                        <?php foreach($slides as $slide){
-										echo "<div>";
-										echo wp_get_attachment_image($slide);
-										echo "</div>";
-                                        }?>
-                                    </div>
-                                </div>
+    				        <?php
+    				        myslider( $post->ID, 'pc' );
+    				        myslider( $post->ID, 'sp' );
+    				        ?>
+        						
                         </div>
                         <div class="slide_title">
                             <p><?php echo the_title(); ?></p>
                         </div>
+
                     </div>
                 </div>
         <?php endwhile;?>
